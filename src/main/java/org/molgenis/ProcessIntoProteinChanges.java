149360cd77103cabc405a3b21b4d45e6d5ca376e
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2023-07-14 16:50:19 +0200

    Java postprocessing tool

---
+++ src/main/java/org/molgenis/ProcessIntoProteinChanges.java
@@ -0,0 +1,104 @@
+package org.molgenis;
+
+import java.io.File;
+import java.io.PrintWriter;
+import java.util.*;
+
+public class ProcessIntoProteinChanges {
+
+    public static void main(String args[]) throws Exception {
+        System.out.println("Starting...");
+
+        File vkglMissense = new File("/Users/joeri/git/vkgl-protein-folding/data/VKGL_apr2023_annot_missense_novus_nodups.vcf");
+        File outputFile = new File("/Users/joeri/git/vkgl-protein-folding/data/VKGL_prot.tsv");
+
+        Map<String,String> uniqClfProtChanges = new TreeMap<>();
+        Set<String> blackListed = new TreeSet<>();
+        Scanner s = new Scanner(vkglMissense);
+        String line;
+        while(s.hasNextLine())
+        {
+            line = s.nextLine();
+            String[] lineSplit = line.split("\t", -1);
+            if(lineSplit.length != 8)
+            {
+                throw new Exception("line split expecting 8 elements for " + line);
+            }
+
+            String[] infoSplit = lineSplit[7].split(";", -1);
+            if(infoSplit.length != 2)
+            {
+                throw new Exception("info split expecting 2 elements for " + line);
+            }
+
+            String[] csqSplit = infoSplit[1].split(",", -1);
+            if(csqSplit.length == 0)
+            {
+                throw new Exception("csq split expecting 1 or more elements for " + line);
+            }
+
+            for(String csq : csqSplit)
+            {
+                String[] csqParts = csq.split("\\|", -1);
+                // CSQ=T|missense_variant|MODERATE|CLIC2|1193|Transcript|NM_001289.6|protein_coding|6/6||||905|718|240|A/T|Gca/Aca|||-1||EntrezGene|||
+                if(csqParts.length != 25)
+                {
+                    throw new Exception("csq parts split expecting 25 elements for " + line);
+                }
+
+                String proteinPos = csqParts[14];
+
+                // some variants are interpreted in a non-coding context in which case proteinPos is empty
+                if(proteinPos.isBlank()){
+                    continue;
+                }
+                String[] proteinChangeSplit = csqParts[15].split("/");
+                // synonymous variants have only 1 letter ('A') instead of 'A/B'
+                if(proteinChangeSplit.length != 2)
+                {
+                    continue;
+                }
+
+                String geneSymbol = csqParts[3];
+                // as per FoldX notation: WT residue, chain, residue number, mutant residue (e.g. "CA1490Y;")
+                String geneProt = geneSymbol + "\t" + proteinChangeSplit[0] + "A" + proteinPos + proteinChangeSplit[1];
+                String clf = infoSplit[0].replace("VKGL=","");
+
+                // unless there is a different protein notation for a transcript, the notation is duplicate
+                // filter here by checking if gene-prot combination is already present
+                // at the same time check for any potential conflicting interpretations for this notation
+                if(uniqClfProtChanges.containsKey(geneProt))
+                {
+                    String prevClf = uniqClfProtChanges.get(geneProt);
+                    if(!prevClf.equals(clf))
+                    {
+                        System.out.println("Difference in classification for key '" + geneProt + "', at " + line);
+                        blackListed.add(geneProt);
+                    }else{
+                        // no difference in classification, so no need to add or do anything
+                    }
+                }else{
+                    // first time add
+                    uniqClfProtChanges.put(geneProt, clf);
+                }
+            }
+        }
+
+        /*
+        Write results, excluding blacklisted protein changes
+         */
+        PrintWriter writer = new PrintWriter(outputFile, "UTF-8");
+        writer.write("Gene" + "\t" + "ProtChange" + "\t" + "Classification" + System.lineSeparator());
+        for(String key : uniqClfProtChanges.keySet())
+        {
+            if(blackListed.contains(key)){
+                continue;
+            }
+            writer.write(key + "\t" + uniqClfProtChanges.get(key) + System.lineSeparator());
+        }
+        writer.flush();
+        writer.close();
+
+    }
+
+}
