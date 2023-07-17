f3df7988fd358d351ec7a02cd4139c892808d68b
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2023-07-17 09:35:51 +0200

    Load coords and added draft for gene plot

--- src/main/R/ProteinFolding.R
+++ src/main/R/ProteinFolding.R
@@ -4,7 +4,12 @@
 #install.packages('R.utils')
 #install.packages('ggplot2')
 #install.packages('reshape2') 
-#install.packages('dplyr') 
+#install.packages('toprdata')
+#install.packages('stringr')
+#install.packages('scales')
+#install.packages('cutpointr')
+#install.packages('plyr')
+#install.packages('dplyr')
 
 #################
 # Load packages #
@@ -12,6 +17,11 @@
 library(R.utils)
 library(ggplot2)
 library(reshape2)
+library(toprdata)
+library(stringr)
+library(scales)
+library(cutpointr)
+library(plyr)
 library(dplyr)
 
 ##############################
@@ -29,7 +39,7 @@ tmpDir <- paste(getwd(), "tmp", sep="/")
 mkdirs(tmpDir)
 geneMappingLoc <- "/Applications/AlphaFold2/hgnc-uniprot-mapping.txt"
 alphaFoldLoc <- "/Applications/AlphaFold2/UP000005640_9606_HUMAN_v4.tar"
-vkglProtLoc <- "/Users/joeri/VKGL/VKGL-prot/VKGL_prot.tsv"
+vkglProtLoc <- "/Users/joeri/VKGL/VKGL-prot/VKGL_apr2023_protForFolding.tsv"
 foldx <- "/Applications/FoldX/foldx5MacStd/foldx_20231231" # seems about 2.5x faster than the C11 version
 
 #################################################
@@ -44,6 +54,7 @@ uniProtID
 ####
 vkglAll <- read.table(file=vkglProtLoc, sep = '\t', header = TRUE)
 vkgl <- subset(vkglAll, Gene == geneName)
+vkgl$Classification <- revalue(vkgl$Classification, c("LB"="LB/B", "VUS"="VUS", "LP"="LP/P"))
 dim(vkgl)
 
 ###
@@ -52,7 +63,7 @@ dim(vkgl)
 alphaFoldAll <- untar(alphaFoldLoc, list = TRUE)
 alphaFoldPDBs <- grep(".pdb.gz",alphaFoldAll,value=TRUE)
 PDBForGeneGz <- grep(uniProtID, alphaFoldPDBs,value=TRUE)
-PDBForGeneGz
+PDBForGeneGz # if more than 1, must select correct fragment (todo)
 untar(alphaFoldLoc, files = PDBForGeneGz)
 PDBForGene <- gunzip(PDBForGeneGz, overwrite=TRUE)[[1]]
 
@@ -67,12 +78,12 @@ repPDBAbsLoc <- paste(getwd(), repPDB, sep="/")
 repPDBAbsLoc
 
 ####
-# Generate individual_list.txt
+# Generate individual_list.txt and fold
 ###
 for(i in 1:nrow(vkgl))
 {
-  cat(paste("Working on", vkgl[i, 2], "(", i, "of", dim(vkgl)[1],")\n", sep=" "))
-  protChangeDir <- paste(tmpDir, vkgl[i, 2], sep="/")
+  cat(paste("Working on", vkgl[i, 7], "(", i, "of", dim(vkgl)[1],")\n", sep=" "))
+  protChangeDir <- paste(tmpDir, vkgl[i, 7], sep="/")
   mkdirs(protChangeDir)
   if(length(list.files(protChangeDir, pattern="*.fxout")) > 0){
     cat("  already folded, skipping...\n")
@@ -81,11 +92,16 @@ for(i in 1:nrow(vkgl))
   cat("  folding...\n")
   file.copy(from = repPDBAbsLoc, to = protChangeDir)
   setwd(protChangeDir)
-  write(paste(vkgl[i,2], ";", sep=""), file = "individual_list.txt")
+  write(paste(vkgl[i,7], ";", sep=""), file = "individual_list.txt")
   system(paste(foldx, " --command=BuildModel --mutant-file=individual_list.txt --pdb=", repPDB, sep=""), intern = TRUE)
   file.remove(repPDB)
   cat("...done!\n")
 }
+
+###
+# TODO cleanup, remove rotabase and model files and such from gene tmp folders
+###
+
 
 #####
 # Gather results
@@ -94,16 +110,22 @@ setwd(tmpDir)
 results <- data.frame()
 for(i in 1:nrow(vkgl))
 {
-  cat(paste("Collecting results from ", vkgl[i, 2], "(", i, "of", dim(vkgl)[1],")\n", sep=" "))
-  protChangeDir <- paste(tmpDir, vkgl[i, 2], sep="/")
+  cat(paste("Collecting results from ", vkgl[i, 7], "(", i, "of", dim(vkgl)[1],")\n", sep=" "))
+  protChangeDir <- paste(tmpDir, vkgl[i, 7], sep="/")
   if(length(list.files(protChangeDir, pattern="*.fxout")) == 0){
     cat("  no results...\n")
     next
   }
   avgDiff <- list.files(protChangeDir, pattern="Average")
   result <- read.table(file = paste(protChangeDir, avgDiff, sep="/"), header = TRUE, skip = 8, sep="\t")
-  result$protChange <- vkgl[i, 2]
-  result$classification <- vkgl[i, 3]
+  result$assembly <- vkgl[i, 1]
+  result$chrom <- vkgl[i, 2]
+  result$pos <- vkgl[i, 3]
+  result$ref <- vkgl[i, 4]
+  result$alt <- vkgl[i, 5]
+  result$gene <- vkgl[i, 6]
+  result$protChange <- vkgl[i, 7]
+  result$classification <- vkgl[i, 8]
   results <- rbind(results, result)
 }
 
@@ -113,7 +135,7 @@ dropCols <- c("Pdb")
 results<- results[ , !(names(results) %in% dropCols)]
 
 # melt dataframe for ggplot
-melted <- melt(results, id = c("protChange","classification")) 
+melted <- melt(results, id = c("assembly", "chrom", "pos", "ref", "alt", "gene", "protChange","classification")) 
 
 
 ####
@@ -124,7 +146,7 @@ setwd(geneWorkingDir)
 
 plotdata <- melted %>%
   group_by(classification, variable) %>%
-  summarize(n = n(),
+  dplyr::summarize(n = n(),
             mean = mean(value),
             sd = sd(value),
             se = sd / sqrt(n))
@@ -142,11 +164,63 @@ ggplot(plotdata,
   theme(legend.position = "none",
         panel.grid.major.x = element_blank(),
         panel.grid.minor.y = element_blank(),
-        strip.text.x = element_text(size = 6)) +
+        strip.text.x = element_text(size = 6),
+        axis.text.x=element_text(size=4)) +
   labs(x="", 
        y="", 
        title=paste("FoldX terms for ", geneName, " based on VKGL variant classifications", sep=""),
        subtitle = "(Means and standard errors, based on VKGL April 2023 public consensus, FoldX 5.0, and AlphaFold2 human proteome v4)") +
-  scale_colour_manual(name = "Classification", values = c("LB" = "#28A014","LP" = "#E41A1C"))
+  scale_colour_manual(name = "Classification", values = c("LB/B" = "#28A014","LP/P" = "#E41A1C"))
 ggsave(paste(geneName,".png",sep=""), width=9, height=5)
+
+
+
+
+
+######
+# Gene plot for 1 attribute
+######
+
+# Retrieve gene and exon coordinates
+geneCoords <- subset(ENSGENES_37, gene_symbol==geneName)
+geneChr <- gsub("chr","", geneCoords$chrom)
+exonCoords <- subset(ENSEXONS_37, gene_symbol==geneName)
+exonStartCoords <- str_split(exonCoords$exon_chromstart, ",")
+exonEndCoords <- str_split(exonCoords$exon_chromend, ",")
+exons <- data.frame(exonStart = as.numeric(unlist(exonStartCoords)), exonEnd = as.numeric(unlist(exonEndCoords)))
+
+# Variable
+unique(melted$variable)
+termName <- "total.energy"
+selectVar <- melted[melted$variable==termName,]
+
+# arrange for plot, LP/P on top
+variants <- variants %>% arrange(factor(classification, levels = c("LB/B","VUS","LP/P")))
+
+
+# Determine optimal threshold using Youden's Index #
+opt_cut <- cutpointr(selectVar, value, classification, direction = ">=", pos_class = "LP/P", neg_class = "LB/B", method = maximize_metric, metric = youden)
+youdenIndex <- opt_cut$optimal_cutpoint
+tp <- sum(selectVar[selectVar$classification=="LP/P",'value'] >= youdenIndex)
+fp <- sum(selectVar[selectVar$classification=="LB/B",'value'] >= youdenIndex)
+ppv <- 100 *tp/(tp+fp)
+sens <- opt_cut$sensitivity*100
+
+# Determine plot window
+xmin <- min(exons$exonStart)
+xmax <- max(exons$exonEnd)
+ymin <- min(selectVar$value)
+ymax <- max(selectVar$value)
+# Create and save plot
+ggplot() +
+  theme_bw() + theme(panel.grid = element_blank(), axis.title.x=element_text(size=10)) +
+  geom_rect(data = exons, aes(xmin = exonStart, xmax = exonEnd, ymin = ymin, ymax = ymax), linetype = 0, fill="lightgray", alpha = 1) +
+  geom_point(data = selectVar, aes(x=pos, y=value, colour=classification), alpha=1.0, size = 2, stroke = 1) +
+  geom_hline(yintercept = youdenIndex) +
+  scale_colour_manual(name = "Classification", values = c("LB/B" = "green","VUS" = "darkgray","LP/P" = "red")) +
+  scale_x_continuous(limits = c(xmin,xmax), labels = comma) +
+  xlab(paste("",geneName," at GRCh37 chr",geneChr,":", xmin, "-",xmax,", lightgray: exons", sep="")) +
+  ylab(termName) +
+  ggtitle(paste("FoldX results for ",geneName,". At a threshold of ",round(youdenIndex, 2), " the PPV is ",round(ppv),"% and the sensitivity is ",round(sens),"%.",sep=""))
+ggsave(paste(geneName,"_",termName,".png",sep=""), width=9, height=5)
 
