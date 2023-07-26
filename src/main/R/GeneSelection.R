c6897b11d3da4ae03c7b49fabd1ee34fdcac3b97
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2023-07-26 16:14:31 +0200

    Add gene select output

--- src/main/R/GeneSelection.R
+++ src/main/R/GeneSelection.R
@@ -34,3 +34,21 @@ genesThr <- subset(genesThr, LP >= threshold)
 genesThr$propVUS <- genesThr$VUS/(genesThr$LB+genesThr$LP)
 sorted <- genesThr[order(-genesThr$propVUS),]
 sorted
+
+#    Gene  LB  LP VUS   propVUS
+#   SCN5A 100 147 495 2.0040486
+#   FGFR3  61  56 155 1.3247863
+# CACNA1A 145  51 159 0.8112245
+#   FGFR1  61 115 133 0.7556818
+#    GNAS  84  79 121 0.7423313
+#    MLH1 152 176 221 0.6737805
+#   SCN1A  87 281 209 0.5679348
+#   ATP7B  84 151 127 0.5404255
+#    SOS1  53  56  55 0.5045872
+#   MUTYH 143 120 113 0.4296578
+#   MECP2  82  82  67 0.4085366
+#    TSC2 649 537 480 0.4047218
+#   BRCA1 596 170 241 0.3146214
+#   FGFR2  74 199  75 0.2747253
+#    LDLR 114 768 225 0.2551020
+#    MAPT  72  69  18 0.1276596 
