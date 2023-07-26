985e276c5b445ec77113bac27d8553eceed81ce8
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2023-07-26 17:49:11 +0200

    More verbose gene coordinate loading

--- src/main/R/load/LoadGenomicCoordinates.R
+++ src/main/R/load/LoadGenomicCoordinates.R
@@ -5,6 +5,8 @@ exonCoordsB37 <- subset(ENSEXONS_37, gene_symbol==geneName)
 exonStartCoordsB37 <- str_split(exonCoordsB37$exon_chromstart, ",")
 exonEndCoordsB37 <- str_split(exonCoordsB37$exon_chromend, ",")
 exonsB37 <- data.frame(exonStart = as.numeric(unlist(exonStartCoordsB37)), exonEnd = as.numeric(unlist(exonEndCoordsB37)))
+cat(paste("Build 37 gene location is ",  geneChrB37, ":", geneCoordsB37$gene_start, "-", geneCoordsB37$gene_end, " with ", dim(exonsB37)[[1]], " exons\n",sep=""))
+
 
 geneCoordsB38 <- subset(ENSGENES, gene_symbol==geneName)
 geneChrB38 <- gsub("chr","", geneCoordsB38$chrom)
@@ -12,4 +14,6 @@ exonCoordsB38 <- subset(ENSEXONS, gene_symbol==geneName)
 exonStartCoordsB38 <- str_split(exonCoordsB38$exon_chromstart, ",")
 exonEndCoordsB38 <- str_split(exonCoordsB38$exon_chromend, ",")
 exonsB38 <- data.frame(exonStart = as.numeric(unlist(exonStartCoordsB38)), exonEnd = as.numeric(unlist(exonEndCoordsB38)))
+cat(paste("Build 38 gene location is ",  geneChrB38, ":", geneCoordsB38$gene_start, "-", geneCoordsB38$gene_end, " with ", dim(exonsB38)[[1]], " exons\n",sep=""))
+
 geneTabixB38 <- paste(geneChrB38, paste(geneCoordsB38$gene_start, geneCoordsB38$gene_end, sep="-"), sep=":")
