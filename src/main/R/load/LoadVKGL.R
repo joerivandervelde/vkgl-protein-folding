95412f980b5f7128d30c4b118466a532b41c6395
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2023-07-27 17:07:50 +0200

    Rework variant data loading

--- src/main/R/load/LoadVKGL.R
+++ src/main/R/load/LoadVKGL.R
@@ -2,8 +2,8 @@
 # VKGL variant classification data
 ####
 
-setwd(dataDir)
 vkglAll <- read.table(file=vkglProtLoc, sep = '\t', header = TRUE)
 vkgl <- subset(vkglAll, Gene == geneName)
 vkgl$Classification <- revalue(vkgl$Classification, c("LB"="LB/B", "VUS"="VUS", "LP"="LP/P"))
+vkgl$source <- "VKGL"
 cat(paste("VKGL data has", dim(vkgl)[[1]], "rows\n", sep=" "))
