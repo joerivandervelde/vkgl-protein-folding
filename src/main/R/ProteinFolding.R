aea8bb090d01575177a139a51e9742f32a0fc597
K. Joeri van der Velde <82420+joerivandervelde@users.noreply.github.com>
2023-08-28 14:03:25 +0200

    Add KCNQ1

--- src/main/R/ProteinFolding.R
+++ src/main/R/ProteinFolding.R
@@ -41,7 +41,7 @@ genes <- c("MEFV", "CFTR", "MECP2", "TERT", "CACNA1A",
            "BRCA1", "FGFR2", "GNAS", "PLCG2", "ABCG8",
            "SCN10A", "KCNH2", "ABCA1", "SCN8A", "PTEN",
            "MET", "RAF1", "F8", "TSC2", "BEST1",
-           "ABCC8")
+           "ABCC8", "KCNQ1")
 # Keep track of results per gene
 columns = c("gene","nbenign","npatho","threshold","ppv","npv","sens","spec","foldingSuccessRate") 
 geneResults = data.frame(matrix(nrow = 0, ncol = length(columns))) 
@@ -66,7 +66,7 @@ foldx <- "/Applications/FoldX/foldx5MacStd/foldx_20231231" # seems about 2.5x fa
 
 for (geneName in genes)
 {
-# geneName <- "ABCC8" # To try out new genes
+# geneName <- "KCNQ1" # To try out new genes
 
 
 ############################
